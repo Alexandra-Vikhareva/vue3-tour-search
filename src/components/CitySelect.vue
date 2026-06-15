c0e0a4fcf5b20addb2c80aca5d5a2cc5a5e841e9
Sasha <st087955@student.spbu.ru>
2026-06-15 16:31:36 +0300

    Содан компонент выпадающего списка с мок данными

---
+++ src/components/CitySelect.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface City {
+    id: number,
+    name: string,
+}
+
+const cities: City[] = [{id: 1, name: 'Москва'}, 
+                        {id: 2, name: 'Санкт-Петербург'},
+                        {id: 3, name: 'Казань'}]
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
