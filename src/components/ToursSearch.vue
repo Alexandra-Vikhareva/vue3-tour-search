c8d9790be369d951cddb8319431e726c2b0efddd
Sasha <st087955@student.spbu.ru>
2026-06-16 17:47:11 +0300

    Добавлены стили и крестик для инпута

--- src/components/ToursSearch.vue
+++ src/components/ToursSearch.vue
@@ -4,10 +4,43 @@ const searchQuery = defineModel<String>()
 </script>
 
 <template>
-    <input 
-        v-model="searchQuery"
-        placeholder="Введите название экскурсии"/>
+    <div class="tour-search">
+        <input 
+            v-model="searchQuery"
+            placeholder="Введите название экскурсии"/>
+
+        <button class="clear-input">✕</button>
+    </div>
+    
 </template>
 
 <style scoped>
+.tour-search {
+    position: relative;
+    display: inline-block;
+    width: 300px;
+    height: 50px;
+}
+.tour-search input{
+    width: 100%;
+    height: 100%;
+    border: 1px solid var(--base-dark-gray);
+    color: var(--base-dark-gray);
+    border-radius: 0;
+    line-height: 21px;
+    font-size: 16px;
+    padding: 0 15px;
+}
+
+.tour-search input:focus {
+    outline: none;
+}
+
+.clear-input {
+    position: absolute;
+    right: 20px;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 14px;
+}
 </style>
